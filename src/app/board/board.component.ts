e8dfd160efce04e7b6383a20c95162714a3133b6
Jacob Gorton <jagorton@iastate.edu>
2023-06-05 16:04:58 -0500

    moving pieces working

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -18,14 +18,19 @@ export class BoardComponent implements OnInit {
   selectedPiece: {x: number, y: number} | null = null;
 
   select(event: number[]){
-    if(this.selectedPiece === null && this.grid[event[0]][event[1]].piece != ''){
-      this.grid[event[0]][event[1]].selected = !this.grid[event[0]][event[1]].selected;
-      this.selectedPiece = {x: event[1], y: event[0]};
+    var x = event[1];
+    var y = event[0];
+    if(this.selectedPiece === null && this.grid[y][x].piece != ''){
+      this.grid[y][x].selected = true;
+      this.selectedPiece = {x: x, y: y};
     }else if(this.selectedPiece !== null){
       this.grid[this.selectedPiece.y][this.selectedPiece.x].selected = false;
-      if(this.grid[event[0]][event[1]].possible){
-        this.grid[event[0]][event[1]].piece = this.grid[this.selectedPiece.y][this.selectedPiece.y].piece;
+      if(this.grid[y][x].possible){
+        // console.log(this.grid[this.selectedPiece.y][this.selectedPiece.x]);
+        // console.log(this.selectedPiece.x,this.selectedPiece.y,"=>",x,y);
+        this.grid[y][x].piece = this.grid[this.selectedPiece.y][this.selectedPiece.x].piece;
         this.grid[this.selectedPiece.y][this.selectedPiece.x].piece = '';
+        // console.log(this.grid[y][x]);
       }
       this.selectedPiece = null;
     }
@@ -33,6 +38,31 @@ export class BoardComponent implements OnInit {
   }
 
   updatePossible(){
+    for(let x = 0; x < this.grid[0].length; x++){
+      for(let y = 0; y < this.grid.length; y++){
+        if(this.selectedPiece === null){
+          this.grid[y][x].possible = false;
+        }else{
+          if(this.reachable(this.selectedPiece.x, this.selectedPiece.y, x, y)){
+            this.grid[y][x].possible = true;
+          }
+        }
+      }
+    }
+  }
 
+  reachable(x1: number, y1: number, x2: number, y2: number){
+    if(x1 === x2 && y1 === y2){
+      return false;
+    }
+
+    var type = this.grid[y1][x1].piece
+    var canReach = true;
+    switch(type){
+      case 'p':
+
+        break;
+    }
+    return canReach;
   }
 }
