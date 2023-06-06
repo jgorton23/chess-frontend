622900768a810498fef2d2dbdebb1d01386fb7e9
Jacob Gorton <jagorton@iastate.edu>
2023-06-06 17:15:58 -0500

    mend

---
+++ src/app/home/side-menu/side-menu.component.ts
@@ -0,0 +1,35 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-side-menu',
+  templateUrl: './side-menu.component.html',
+  styleUrls: ['./side-menu.component.css'],
+  animations: [
+    trigger('openClose', [
+      state('left', style({ transform: 'translateX(-100%)' })),
+      state('right', style({ transform: 'translateX(0%)' })),
+      transition('left => right', [
+        animate(125, style({ transform: 'translateX(0%)' })),
+      ]),
+      transition('right => left', [
+        animate(125, style({ transform: 'translateX(-100%)' })),
+      ]),
+    ]),
+  ]
+})
+export class SideMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  @Input()
+  isOpen: boolean = false;
+
+  // toggle() {
+  //   this.isOpen = !this.isOpen;
+  // }
+
+}
