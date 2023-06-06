20dcc43978dfe388c98267792367e670852a6197
Jacob Gorton <jagorton@iastate.edu>
2023-06-06 14:41:15 -0500

    register style copied login style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { BoardComponent } from './board/board.component';
 
 const routes: Routes = [
-  { path: 'register', component: RegisterComponent},
   { path: 'login', component: LoginComponent},
+  { path: '', redirectTo: 'login', pathMatch: 'full'},
+  { path: 'register', component: RegisterComponent},
+  { path: 'board', component: BoardComponent},
 ];
 
 @NgModule({
