6e057acb4109d807db378e36c840217194ea38f7
Jacob Gorton <jagorton@iastate.edu>
2023-06-14 14:42:30 -0500

    added environment variable for baseUrl

---
+++ src/app/api-paths.ts
@@ -0,0 +1,5 @@
+export enum ApiPaths {
+    Login = '/auth/login',
+    Register = '/auth/register',
+    
+}
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { ApiPaths } from '../api-paths';
 
 @Component({
   selector: 'app-register',
@@ -13,10 +15,27 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  register(formData: {email: String, password: String, passwordConfirm: String}) {
-    console.log(formData.email);
-    console.log(formData.password);
-    console.log(formData.passwordConfirm);
-    this.router.navigate(['login'])
+  async register(formData: {email: String, password: String, confirm: String}) {
+    try {
+      const params = {
+        headers: {
+          'Accept': "application/json, text/plain, */*",
+          'Content-Type': "application/json;charset=utf-8"
+        },
+        method: 'POST',
+        body: JSON.stringify({...formData, username:formData.email.split("@")[0]})
+      }
+      const response = await fetch(`${environment.baseUrl}/${ApiPaths.Register}`, params)
+      const status = response.status
+      if(status === 200){
+        this.router.navigate(['login'])
+      } else {
+        console.log(response.body);
+        
+        console.log(await response.json());
+      }
+    } catch(e) {
+      console.log(e);
+    }
   }
 }
