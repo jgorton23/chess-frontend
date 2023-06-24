3b33c9f495416f2b74f70805c330213852305757
Jacob Gorton <jagorton@iastate.edu>
2023-06-24 14:06:43 -0500

    websocket service

--- src/app/websocket/websocket-api.service.ts
+++ src/app/websocket/websocket-api.service.ts
@@ -4,6 +4,29 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class WebsocketAPIService {
+  wsEndpoint: string = 'http://localhost:8080/websocket'
+  topic: string = '/game'
 
   constructor() { }
+
+  _connect() {
+  
+  }
+
+  _disconnect() {
+
+  }
+
+  _send(message: string) {
+
+  }
+
+  _error(error: Error) {
+
+  }
+
+  onMessageRecieved(message: string) {
+
+  }
 }
+
